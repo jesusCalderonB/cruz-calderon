058e786e75d19722be8225d94df77349923d95d1
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro.local>
2019-11-29 00:28:53 -0600

    sliders home y footer

--- playeras-app/src/app/app-routing.module.ts
+++ playeras-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TopBarComponent} from './top-bar/topbar.component'
+import {TopBarComponent} from './top-bar/topbar.component';
+import {FooterComponent} from './footer/footer.component'
 
 
 const routes: Routes = [
   {path: './top-bar/topbar.component',component: TopBarComponent},
+  {path: './footer/footer.component',component: FooterComponent},
 ];
 
 @NgModule({
--- playeras-app/src/app/app.module.ts
+++ playeras-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/topbar.component';
+import {FooterComponent} from './footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
